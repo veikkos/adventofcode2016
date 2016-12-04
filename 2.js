6738dddb372f12dc70d589808c4372c783b50351
Veikko Soininen <veikko.soininen@saunalahti.fi>
2016-12-04 19:51:33 +0200

    Task 2b.

--- 2.js
+++ 2.js
@@ -1,39 +1,57 @@
 'use strict';
 
-function parseInstructions(instructions) {
-    var pos = {
-        x : 1,
-        y : 1
-    };
+function limitA(p) {
+    if (p > 2) {
+        return 2;
+    } else if (p < 0) {
+        return 0;
+    } else {
+        return p;
+    }
+}
+
+function limitB(p, start) {
+    if (p > 4) {
+        return 4;
+    } else if (p < 0) {
+        return 0;
+    } else {
+        return p;
+    }
+}
+
+const customPad = [
+    ['X', 'X', '1', 'X', 'X'],
+    ['X', '2', '3', '4', 'X'],
+    ['5', '6', '7', '8', '9'],
+    ['X', 'A', 'B', 'C', 'X'],
+    ['X', 'X', 'D', 'X', 'X']
+];
+
+function parseInstructions(instructions, pos, custom) {
     var code = '';
     var lines = instructions.split('\n');
     lines.forEach(function (line) {
         if(line.length) {
-            code += parseLine(pos, line).toString();
+            code += parseLine(pos, line, custom);
         }
     });
     return code;
 }
 
-function parseLine(pos, line) {
+function parseLine(pos, line, custom) {
     for(var index in line) {
-        pos = move(pos, line[index]);
+        pos = move(pos, line[index], custom);
     }
 
-    return getDigit(pos);
-};
+    return custom ?
+        customPad[4 - pos.y][pos.x] :
+        getDigit(pos).toString();
+}
 
-function move(pos, dir) {
-    function limit(p) {
-        if (p > 2) {
-            return 2;
-        } else if (p < 0) {
-            return 0;
-        } else {
-            return p;
-        }
-    }
-
+function move(pos, dir, custom) {
+    var limit = custom ? limitB : limitA;
+    var orig = JSON.parse(JSON.stringify(pos));
     switch (dir) {
     case 'U':
         pos.y = limit(pos.y + 1); break;
@@ -47,6 +65,12 @@ function move(pos, dir) {
         console.error('Unknown dir', dir);
     }
 
+    if (custom) {
+        if (customPad[4 - pos.y][pos.x] === 'X') {
+            pos.x = orig.x;
+            pos.y = orig.y;
+        }
+    }
     return pos;
 }
 
--- test/2.test.js
+++ test/2.test.js
@@ -7,28 +7,79 @@ var readFile = Promise.promisify(require("fs").readFile);
 var two = require('../2');
 
 describe('2', function() {
-    it('should get digit 1', function() {
-        var pos = {
-            x : 1,
-            y : 1
-        };
-        two.parseLine(pos, 'ULL').should.be.equal(1);
-    });
+    describe('a', function() {
+        it('should get digit 1', function() {
+            var pos = {
+                x : 1,
+                y : 1
+            };
+            two.parseLine(pos, 'ULL').should.be.equal('1');
+        });
 
-    it('should get code 1985', function() {
-        two.parseInstructions('ULL\nRRDDD\nLURDL\nUUUUD\n')
-            .should.be.equal('1985');
-    });
+        it('should get code 1985', function() {
+            var pos = {
+                x : 1,
+                y : 1
+            };
+            two.parseInstructions('ULL\nRRDDD\nLURDL\nUUUUD\n',
+                                  pos)
+                .should.be.equal('1985');
+        });
 
-    it('should get task done', function() {
-        return readFile('2_input.txt', 'utf8')
-            .then(function(input) {
-                two.parseInstructions(input)
-                    .should.be.equal('65556');
-            })
-            .catch(function(e) {
-                console.error(e);
-                false.should.equal(true);
-            });
+        it('should get task done', function() {
+            return readFile('2_input.txt', 'utf8')
+                .then(function(input) {
+                    var pos = {
+                        x : 1,
+                        y : 1
+                    };
+                    two.parseInstructions(input,
+                                          pos)
+                        .should.be.equal('65556');
+                })
+                .catch(function(e) {
+                    console.error(e);
+                    false.should.equal(true);
+                });
+        });
+    });
+    describe('b', function() {
+        it('should get digit 2', function() {
+            var pos = {
+                x : 2,
+                y : 2
+            };
+            two.parseLine(pos, 'ULL', true).
+                should.be.equal('2');
+        });
+
+        it('should get code 5DB3', function() {
+            var pos = {
+                x : 0,
+                y : 2
+            };
+            two.parseInstructions('ULL\nRRDDD\nLURDL\nUUUUD\n',
+                                  pos,
+                                  true)
+                .should.be.equal('5DB3');
+        });
+
+        it('should get task done', function() {
+            return readFile('2_input.txt', 'utf8')
+                .then(function(input) {
+                    var pos = {
+                        x : 1,
+                        y : 1
+                    };
+                    two.parseInstructions(input,
+                                          pos,
+                                          true)
+                        .should.be.equal('CB779');
+                })
+                .catch(function(e) {
+                    console.error(e);
+                    false.should.equal(true);
+                });
+        });
     });
 });
