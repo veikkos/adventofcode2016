9e8e92bfa2e809d508e295bbb762ee3fdf20aea8
Veikko Soininen <veikko.soininen@saunalahti.fi>
2016-12-06 17:05:35 +0200

    Task 5b.

--- 5.js
+++ 5.js
@@ -2,15 +2,20 @@
 
 var crypto = require('crypto');
 
-function getDigits(id, len) {
+function getHash(input) {
+    return crypto.createHash('md5').update(input).digest("hex");
+}
+
+function getDigits(id) {
     var index = 0,
         hash,
         code = '';
 
-    for(var i=0; i<len; ++i) {
+    for (var i=0; i<8; ++i) {
         do {
-            hash = crypto.createHash('md5').update(id + index++).digest("hex");
-        } while (!hash.startsWith('00000'));
+            hash = getHash(id + index++);
+        }
+        while (!hash.startsWith('00000'));
 
         code += hash[5];
     }
@@ -18,6 +23,31 @@ function getDigits(id, len) {
     return code;
 }
 
+function getAdvancedDigits(id) {
+    var index = 0,
+        hash,
+        code = new Array(8),
+        pos;
+
+    code.fill(0);
+
+    do {
+        do {
+            hash = getHash(id + index++);
+        } while (!hash.startsWith('00000'));
+
+        pos = parseInt(hash[5], 16);
+
+        if (pos < code.length && code[pos] === 0) {
+            code[pos] = hash[6];
+        }
+    }
+    while(code.indexOf(0) != -1);
+
+    return code.join('');
+}
+
 module.exports = {
-    getDigits
+    getDigits,
+    getAdvancedDigits
 };
--- test/5.test.js
+++ test/5.test.js
@@ -9,11 +9,23 @@ describe('5', function() {
         this.timeout(60000);
 
         it('should solve example', function() {
-            five.getDigits('abc', 8).should.be.equal('18f47a30');
+            five.getDigits('abc').should.be.equal('18f47a30');
         });
 
         it('should solve task', function() {
-            five.getDigits('ffykfhsq', 8).should.be.equal('c6697b55');
+            five.getDigits('ffykfhsq').should.be.equal('c6697b55');
+        });
+    });
+
+    describe('b', function() {
+        this.timeout(120000);
+
+        it('should solve example', function() {
+            five.getAdvancedDigits('abc').should.be.equal('05ace8e3');
+        });
+
+        it('should solve task', function() {
+            five.getAdvancedDigits('ffykfhsq').should.be.equal('8c35d1ab');
         });
     });
 });
